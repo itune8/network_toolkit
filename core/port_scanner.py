1b49ad4aed2c6c957f7c544f342d5aa293bac7ee
itune8 <sahilgulmohur@gmail.com>
2025-08-24 20:38:29 +0530

    Start port scanner module with common ports

---
+++ core/port_scanner.py
@@ -0,0 +1,9 @@
+"""TCP port scanner with service detection."""
+
+import socket
+import time
+
+COMMON_PORTS = {
+    21: "FTP", 22: "SSH", 23: "Telnet", 25: "SMTP", 53: "DNS",
+    80: "HTTP", 110: "POP3", 143: "IMAP", 443: "HTTPS", 445: "SMB",
+}
