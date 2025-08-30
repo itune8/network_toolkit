0bcb9de6a0475b11396e067bb8b7a16220e83604
itune8 <sahilgulmohur@gmail.com>
2025-08-30 21:08:13 +0530

    Expand common ports list with database and admin ports

--- core/port_scanner.py
+++ core/port_scanner.py
@@ -6,4 +6,7 @@ import time
 COMMON_PORTS = {
     21: "FTP", 22: "SSH", 23: "Telnet", 25: "SMTP", 53: "DNS",
     80: "HTTP", 110: "POP3", 143: "IMAP", 443: "HTTPS", 445: "SMB",
+    993: "IMAPS", 995: "POP3S", 3306: "MySQL", 3389: "RDP",
+    5432: "PostgreSQL", 5900: "VNC", 6379: "Redis", 8080: "HTTP-Alt",
+    8443: "HTTPS-Alt", 27017: "MongoDB",
 }
